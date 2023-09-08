022aac5fb3b2627839f43b0cd91ddf75f626de79
Matthew Grimm <kogcyc@gmail.com>
2023-09-08 09:20:09 -0700

    added email

--- process.py
+++ process.py
@@ -9,6 +9,8 @@ with open(file_path, "a") as file:
     file.write("This is a new line added by process.py\n")
 
 # Commit changes using Git within the GitHub Action
+os.system("git config --global user.email 'kogcyc@gmail.com'")
+os.system("git config --global user.name 'Matthew Grimm'")
 os.system("git add .")
-os.system(f'git commit -m "Modify file using GitHub Action"')
+os.system("git commit -m 'Modify file using GitHub Action'")
 os.system("git push origin main")
